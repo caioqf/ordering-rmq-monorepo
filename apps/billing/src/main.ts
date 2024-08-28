e0f24fc4f18396c90eb7aa163545a68c5292390c
caioqf <caio.faria@realizati.com.br>
2024-08-28 18:45:42 -0300

    add: criação dinâmica de um modulo RMQ

--- apps/billing/src/main.ts
+++ apps/billing/src/main.ts
@@ -5,7 +5,8 @@ import { RmqService } from '@app/common/rmq/rmq.service';
 async function bootstrap() {
   const app = await NestFactory.create(BillingModule);
   const rmqService = app.get<RmqService>(RmqService)
-  
-  await app.listen(3000);
+  app.connectMicroservice(rmqService.getOptions("BILLING", false)) // TODO -> testar o noAck: true depois
+
+  await app.startAllMicroservices();
 }
 bootstrap();
--- libs/common/src/rmq/rmr.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@nestjs/common";
-import { RmqService } from "./rmq.service";
-
-@Module({
-  providers: [RmqService]
-})
-export class RmqModule { }
